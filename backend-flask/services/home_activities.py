02311090c06f17685d011576d96da99985f0f16e
pelumikolade <pelumi.kolade@gmail.com>
2023-03-05 22:53:34 +0000

    added script for x-ray

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -7,7 +7,7 @@ class HomeActivities:
     with tracer.start_as_current_span("home-activities-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat)
+      span.set_attribute("app.now", now.isoformat())
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
@@ -47,6 +47,6 @@ class HomeActivities:
         'replies': []
       }
       ]
-      span.set_attribute("app.result_length", result.len(results))
+      span.set_attribute("app.result_length", len(results))
       return results
       
